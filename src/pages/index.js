8d2ddaa3a3c40f2954f5ec02057c05e8ffeaf7b3
Jonathan Beri <jmberi@gmail.com>
2021-06-01 16:44:01 -0700

    Better fav and title

--- src/pages/index.js
+++ src/pages/index.js
@@ -62,7 +62,7 @@ function Home() {
   const { siteConfig = {} } = context;
   return (
     <Layout
-      title={siteConfig.title}
+      title="Docs homepage"
       description="Golioth: the IoT cloud platform built for hardware"
     >
       <header className={clsx("hero hero--primary", styles.heroBanner)}>
