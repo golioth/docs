b81e60a7e9a8dd51da6883be91a85249e14725bd
Chris Gammell <chris.gammell@gmail.com>
2021-06-01 19:51:06 -0500

    Change destination of banner button on docs homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,7 +71,7 @@ function Home() {
                 "button button--outline button--secondary button--lg",
                 styles.getStarted
               )}
-              to={useBaseUrl("docs/guides")}
+              to={useBaseUrl("docs/guides/quickstart/getting-started")}
             >
               Try Our Quickstart Guide
             </Link>
