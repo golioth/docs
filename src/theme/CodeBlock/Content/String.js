075454fdd4cb08f16b4f2000f6dfda590b40396c
Mike Szczys <mike@golioth.io>
2026-05-29 10:24:23 -0500

    reswizzle CodeBlock to match newer Docusuarus CodeBlock

--- src/theme/CodeBlock/Content/String.js
+++ src/theme/CodeBlock/Content/String.js
@@ -1,113 +1,62 @@
 import React from 'react';
-import clsx from 'clsx';
-import {useThemeConfig, usePrismTheme} from '@docusaurus/theme-common';
+import {useThemeConfig} from '@docusaurus/theme-common';
 import {
-  parseCodeBlockTitle,
-  parseLanguage,
-  parseLines,
-  containsLineNumbers,
+  CodeBlockContextProvider,
+  createCodeBlockMetadata,
   useCodeWordWrap,
 } from '@docusaurus/theme-common/internal';
-import {Highlight} from 'prism-react-renderer';
-import Line from '@theme/CodeBlock/Line';
-import CopyButton from '@theme/CodeBlock/CopyButton';
-import WordWrapButton from '@theme/CodeBlock/WordWrapButton';
 import Container from '@theme/CodeBlock/Container';
+import Content from '@theme/CodeBlock/Content';
+import Buttons from '@theme/CodeBlock/Buttons';
+import Title from '@theme/CodeBlock/Title';
 import styles from './styles.module.css';
 import SvgUseThisPipeline from '@site/static/img/usethispipeline.svg';
-// Prism languages are always lowercase
-// We want to fail-safe and allow both "php" and "PHP"
-// See https://github.com/facebook/docusaurus/issues/9012
-function normalizeLanguage(language) {
-  return language?.toLowerCase();
-}
+
 export default function CodeBlockString({
   children,
-  className: blockClassName = '',
+  className,
   metastring,
   title: titleProp,
-  pipeline_share_link: pipeline_share_linkProp,
+  pipeline_share_link: pipelineShareLinkProp,
   showLineNumbers: showLineNumbersProp,
   language: languageProp,
 }) {
-  const {
-    prism: {defaultLanguage, magicComments},
-  } = useThemeConfig();
-  const language = normalizeLanguage(
-    languageProp ?? parseLanguage(blockClassName) ?? defaultLanguage,
-  );
-  const prismTheme = usePrismTheme();
-  const wordWrap = useCodeWordWrap();
-  // We still parse the metastring in case we want to support more syntax in the
-  // future. Note that MDX doesn't strip quotes when parsing metastring:
-  // "title=\"xyz\"" => title: "\"xyz\""
-  const title = parseCodeBlockTitle(metastring) || titleProp;
-  const pipeline = pipeline_share_linkProp;
-  const {lineClassNames, code} = parseLines(children, {
+  const {prism} = useThemeConfig();
+  const metadata = createCodeBlockMetadata({
+    code: children,
+    className,
     metastring,
-    language,
-    magicComments,
+    magicComments: prism.magicComments,
+    defaultLanguage: prism.defaultLanguage,
+    language: languageProp,
+    title: titleProp,
+    showLineNumbers: showLineNumbersProp,
   });
-  const showLineNumbers =
-    showLineNumbersProp ?? containsLineNumbers(metastring);
+  const wordWrap = useCodeWordWrap();
+  const pipeline = pipelineShareLinkProp;
+
   return (
-    <Container
-      as="div"
-      className={clsx(
-        blockClassName,
-        language &&
-          !blockClassName.includes(`language-${language}`) &&
-          `language-${language}`,
-      )}>
-      {title &&
-        <div className={styles.codeBlockTitleBlock}>
-          <div className={styles.codeBlockTitle}>{title}</div>
-          {pipeline &&
-            <div className={styles.codeBlockTitleRight}>
-              <a href={pipeline} target="_blank">
-              <SvgUseThisPipeline className={styles.useThisPipelineSvg} />
-              </a>
-         </div>}
-        </div>}
-      <div className={styles.codeBlockContent}>
-        <Highlight theme={prismTheme} code={code} language={language ?? 'text'}>
-          {({className, style, tokens, getLineProps, getTokenProps}) => (
-            <pre
-              /* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */
-              tabIndex={0}
-              ref={wordWrap.codeBlockRef}
-              className={clsx(className, styles.codeBlock, 'thin-scrollbar')}
-              style={style}>
-              <code
-                className={clsx(
-                  styles.codeBlockLines,
-                  showLineNumbers && styles.codeBlockLinesWithNumbering,
-                )}>
-                {tokens.map((line, i) => (
-                  <Line
-                    key={i}
-                    line={line}
-                    getLineProps={getLineProps}
-                    getTokenProps={getTokenProps}
-                    classNames={lineClassNames[i]}
-                    showLineNumbers={showLineNumbers}
-                  />
-                ))}
-              </code>
-            </pre>
-          )}
-        </Highlight>
-        <div className={styles.buttonGroup}>
-          {(wordWrap.isEnabled || wordWrap.isCodeScrollable) && (
-            <WordWrapButton
-              className={styles.codeButton}
-              onClick={() => wordWrap.toggle()}
-              isEnabled={wordWrap.isEnabled}
-            />
-          )}
-          <CopyButton className={styles.codeButton} code={code} />
+    <CodeBlockContextProvider metadata={metadata} wordWrap={wordWrap}>
+      <Container as="div" className={metadata.className}>
+        {metadata.title && (
+          <div className={styles.codeBlockTitleBlock}>
+            <div className={styles.codeBlockTitle}>
+              <Title>{metadata.title}</Title>
+            </div>
+            {pipeline && (
+              <div className={styles.codeBlockTitleRight}>
+                <a href={pipeline} target="_blank" rel="noreferrer">
+                  <SvgUseThisPipeline className={styles.useThisPipelineSvg} />
+                </a>
+              </div>
+            )}
+          </div>
+        )}
+        <div className={styles.codeBlockContent}>
+          <Content />
+          <Buttons />
         </div>
-      </div>
-    </Container>
+      </Container>
+    </CodeBlockContextProvider>
   );
 }
